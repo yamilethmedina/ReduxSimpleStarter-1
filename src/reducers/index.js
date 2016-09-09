3a0336c4839de6bcd07cdd9c7cbdb30ea4facc0f
Yami Medina <yamilethmedina1@gmail.com>
2016-09-09 13:48:08 -0400

    initial commit with book reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+// mapping of the state
 const rootReducer = combineReducers({
   state: (state = {}) => state
+  books: booksReducer
 });
 
 export default rootReducer;
